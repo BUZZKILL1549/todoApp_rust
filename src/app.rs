c537cdc6398824703d7ef6566052f1ad46d84ac7
BUZZKILL1549 <aushvs10@hotmail.com>
2025-03-17 18:31:41 +0530

    Basic Functionality

---
+++ src/app.rs
@@ -0,0 +1,60 @@
+use serde::{Deserialize, Serialize};
+use serde_json;
+use std::fs::OpenOptions;
+use std::io::Error;
+
+const FILE_PATH: &str = "/home/buzzkill/Documents/Projects/rust/todo/todoFile.json";
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Todo {
+    pub name: String,
+    pub priority: u8,
+    pub completed: bool,
+}
+
+impl Todo {
+    pub fn new(name: String, priority: u8, completed: bool) -> Result<Self, Error> {
+        Self::create_if_not_present().expect("Failed to create file.");
+
+        Ok(Self {
+            name,
+            priority,
+            completed,
+        })
+    }
+
+    pub fn read_from_file() -> Result<Vec<Todo>, Error> {
+        Self::create_if_not_present()?;
+
+        let file_content = std::fs::read_to_string(FILE_PATH)?;
+        if file_content.is_empty() {
+            return Ok(Vec::new());
+        }
+
+        let content: Vec<Todo> = serde_json::from_str(&file_content).unwrap_or_else(|_| Vec::new());
+
+        Ok(content)
+    }
+
+    pub fn write_to_file(&self) -> Result<(), Error> {
+        Self::create_if_not_present()?;
+
+        let mut content = Self::read_from_file()?;
+
+        content.push(self.clone());
+
+        let json = serde_json::to_string(&content)?;
+        std::fs::write(FILE_PATH, json)?;
+
+        Ok(())
+    }
+    
+    fn create_if_not_present() -> Result<(), Error> {
+        OpenOptions::new()
+            .create(true)
+            .append(true)
+            .open(FILE_PATH)?;
+
+        Ok(())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use clap::Parser;
+
+mod app;
+use app::*;
+
+/// Simple ToDo program
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Name of the activity
+    #[arg(short, long)]
+    name: String,
+
+    /// Priority of the activity
+    #[arg(short, long, default_value_t = 1)]
+    priority: u8,
+
+    /// Status of activity
+    #[arg(short, long, default_value_t = false)]
+    completed: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    let new_setup =
+        Todo::new(args.name, args.priority, args.completed).expect("Failed to create file.");
+
+    new_setup.write_to_file().expect("Failed to write to file.");
 }
